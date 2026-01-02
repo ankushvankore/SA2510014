d0161960920eceb7079e9b7b4c39b950372440f4
Ankush <ankushvankore@gmail.com>
2026-01-02 22:42:20 +0530

    Date: 02 Jan 2026

---
+++ MySeleniumAutomationProject/src/com/WebDriverDemos/D02ReadTheTitle.java
@@ -0,0 +1,24 @@
+package com.WebDriverDemos;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class D02ReadTheTitle {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.selenium.dev/downloads/");
+		
+		String title = driver.getTitle();
+		System.out.println("Title: " + title);
+		
+		if(title.equals("Downloads | Selenium"))
+			System.out.println("Test case pass");
+		else
+			System.out.println("Test case fail");
+		
+		driver.close();
+	}
+
+}
